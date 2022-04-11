d41390d617fd684090ac5bd800ec34c52a14dfc0
Hijulmk <hijulsuresh@gmail.com>
2022-04-11 11:37:00 +0530

    logo inseted

---
+++ lib/Pages/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Hm extends StatefulWidget {
+  const Hm({Key? key}) : super(key: key);
+
+  @override
+  State<Hm> createState() => _HmState();
+}
+
+class _HmState extends State<Hm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:prescrible/Pages/sign_up_page.dart';
+import 'package:prescrible/Widgets/my_button.dart';
 
 class LoginPg extends StatefulWidget {
   const LoginPg({Key? key}) : super(key: key);
@@ -7,6 +11,9 @@ class LoginPg extends StatefulWidget {
   State<LoginPg> createState() => _LoginPgState();
 }
 
+final username = TextEditingController();
+final passwrd = TextEditingController();
+
 class _LoginPgState extends State<LoginPg> {
   @override
   Widget build(BuildContext context) {
@@ -14,9 +21,42 @@ class _LoginPgState extends State<LoginPg> {
         appBar: AppBar(
           title: Text("PRESCRIBLE"),
         ),
-        body: ElevatedButton(
-          child: Text('Login'),
-          onPressed: () {},
-        ));
+        body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Container(height: 150,width: 150,child: Image.asset("assets/images/Logo.png"),),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: username,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: passwrd,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return SignUpPg();
+                }));
+              },
+              child: Text("New User? Create Account"),),
+          ElevatedButton(
+            child: Text('Login'),
+            onPressed: () {},
+          ),MyButton("Test")
+        ],
+      ),);
   }
 }
